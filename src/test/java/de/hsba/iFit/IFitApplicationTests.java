984b6dca69d7a3acf8511f6bba316e03668981b7
Jurij Befus <jurij.befus@gmail.com>
2019-12-22 16:36:23 +0100

    clean re-init

---
+++ src/test/java/de/hsba/iFit/IFitApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsba.iFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
