29cbd1cbb16845cffd9982fef6ba47de98b1e126
Jurij Befus <jurij.befus@gmail.com>
2019-12-30 06:16:22 +0100

    security update

--- src/main/java/de/hsba/iFit/web/UserController.java
+++ src/main/java/de/hsba/iFit/web/UserController.java
@@ -1,8 +1,13 @@
 package de.hsba.ifit.web;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //Enthälten allgemeine Routen, die keinem Controller direkt zuzuordnen sind.
 @Controller
@@ -12,6 +17,21 @@ public class UserController {
     public String login(Model model) {
 
         return "user/login";
+    }
+
+    // Gibt Login-Ansicht zurück. Stellt Fehlermeldung bei fehlgeschlagenem Login
+    // dar.
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) boolean error, Model model,
+            @RequestParam(required = false) Integer id) {
+        String msg = error ? "Bitte Passwort und Nutzernamen prüfen!" : "";
+        model.addAttribute("err", error);
+        model.addAttribute("msg", msg);
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        return auth instanceof AnonymousAuthenticationToken ? "user/login" : "redirect:/";
+
     }
 
     @RequestMapping("/user/appointments")
@@ -25,7 +45,5 @@ public class UserController {
 
         return "user/settings";
     }
-
-
 
 }
