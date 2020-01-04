e88c3dfd05c77fc19f67854b83171ab7775b8b4e
Niklas Eick <niklas.eick@myhsba.de>
2020-01-04 16:40:19 +0100

    3_frontend_designarbeiten

--- src/main/java/de/hsba/iFit/web/HomeController.java
+++ src/main/java/de/hsba/iFit/web/HomeController.java
@@ -1,12 +1,19 @@
 package de.hsba.ifit.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.hsba.ifit.course.CourseRepository;
 
 //Enthälten allgemeine Routen, die keinem Controller direkt zuzuordnen sind.
 @Controller
 public class HomeController {
+
+    @Autowired
+    private CourseRepository courseRepository;
 
     @RequestMapping("/")
     public String root() {
@@ -23,6 +30,13 @@ public class HomeController {
     public String classTermine(Model model) {
 
         return "kurs-termine";
+    }
+
+    // Gibt Listenansicht (Plan) der Kurse zurück
+    @GetMapping("/course/plan")
+    public String showAllCourses(Model model) {
+        model.addAttribute("courses", courseRepository.findAll());
+        return "kursplan";
     }
 
 }
