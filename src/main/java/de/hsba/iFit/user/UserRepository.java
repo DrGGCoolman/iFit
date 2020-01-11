5fa10a3a368240da7a2a7a3cb96e471300a192d7
DrGGCoolman <33312626+DrGGCoolman@users.noreply.github.com>
2020-01-11 13:17:19 +0100

    trainer kursselektion wird nun auch beachtet

--- src/main/java/de/hsba/iFit/user/UserRepository.java
+++ src/main/java/de/hsba/iFit/user/UserRepository.java
@@ -17,7 +17,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     List<User> findAll();
 
-    List<User> findBySlotsId(Integer id);
+    List<User> findBySlotsIdAndCoursesId(Integer slotId, Integer courseId);
 
     Optional<User> findById(Integer id);
 
